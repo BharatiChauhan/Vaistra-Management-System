302be8fd131e28e7e139829dfb52fe6b1017d977
BharatiChauhan <chauhanbharati9951@gmail.com>
2023-09-23 15:16:53 +0530

    commit

--- src/main/java/com/vaistramanagement/vaistramanagement/controller/AuthenticationController.java
+++ src/main/java/com/vaistramanagement/vaistramanagement/controller/AuthenticationController.java
@@ -5,12 +5,16 @@ import com.vaistramanagement.vaistramanagement.config.security.AuthenticationReq
 import com.vaistramanagement.vaistramanagement.config.security.AuthenticationResponse;
 import com.vaistramanagement.vaistramanagement.config.security.RegisterRequest;
 import com.vaistramanagement.vaistramanagement.service.AuthenticationService;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -29,5 +33,13 @@ public class AuthenticationController
     public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody AuthenticationRequest request)
     {
         return ResponseEntity.ok(service.authenticate(request));
+    }
+
+    @PostMapping("/refresh-token")
+    public void refreshToken(
+            HttpServletRequest request,
+            HttpServletResponse response
+    ) throws IOException {
+        service.refreshToken(request, response);
     }
 }
